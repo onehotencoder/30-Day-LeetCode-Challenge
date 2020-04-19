2cc30cf9659e2d16d620abdfcc28d3309fb827f7
unknown <sujith.horakeri@maersk.com>
2020-04-19 13:46:09 +0530

    added day 20 solution

--- src/Day13ContiguousArray.java
+++ src/Day13ContiguousArray.java
@@ -1,26 +1,25 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Day13ContiguousArray {
 	public int findMaxLength(int[] nums) {
-		HashMap<Integer, Integer> map = new HashMap<>();
-
-		int zero = 0;
-		int one = 0;
-		int len = 0;
 		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] == 0) {
-				zero++;
-			} else {
-				one++;
-			}
-
-			if (map.containsKey(zero - one)) {
-				len = Math.max(len, i - map.get(zero - one));
-			} else {
-				map.put(zero - one, i);
-			}
+			if (nums[i] == 0)
+				nums[i] = -1;
 		}
 
-		return len;
+		Map<Integer, Integer> sumToIndex = new HashMap<>();
+		sumToIndex.put(0, -1);
+		int sum = 0, max = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (sumToIndex.containsKey(sum)) {
+				max = Math.max(max, i - sumToIndex.get(sum));
+			} else {
+				sumToIndex.put(sum, i);
+			}
+		}
+		return max;
 	}
 }
